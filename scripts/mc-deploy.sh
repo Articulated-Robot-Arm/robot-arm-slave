c0657221f918e987d70b09c5f9e20b8d2e05cedd
carwyn987 <carwyncollinsworth@gmail.com>
2025-04-02 22:20:21 -0700

    Add venv requirements

--- scripts/mc-deploy.sh
+++ scripts/mc-deploy.sh
@@ -6,6 +6,7 @@ ip_list=()
 while IFS= read -r ip; do
     ip_list+=("$ip")
 done < <(sudo arp-scan --interface=enp36s0 --localnet | awk '/^[0-9]/ && !/Routerboard\.com/ {print $1}' | grep -E '^([0-9]{1,3}\.){3}[0-9]{1,3}$')
+# sudo arp-scan --interface=enp36s0 --localnet | awk '/^[0-9]/ && !/Routerboard\.com/ {print $1}' | grep -E '^([0-9]{1,3}\.){3}[0-9]{1,3}$'
 
 # Print the list to verify
 for ip in "${ip_list[@]}"; do
