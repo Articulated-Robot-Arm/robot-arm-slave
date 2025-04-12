1c314ca785259f0cfa99276e7f7d9ccbb42dd4ea
carwyn987 <carwyncollinsworth@gmail.com>
2025-04-11 17:49:18 -0700

    Remove sudo

--- scripts/mc-deploy.sh
+++ scripts/mc-deploy.sh
@@ -5,7 +5,7 @@
 ip_list=()
 while IFS= read -r ip; do
     ip_list+=("$ip")
-done < <(sudo arp-scan --interface=enp36s0 --localnet | awk '/^[0-9]/ && !/Routerboard\.com/ {print $1}' | grep -E '^([0-9]{1,3}\.){3}[0-9]{1,3}$')
+done < <(arp-scan --interface=enp36s0 --localnet | awk '/^[0-9]/ && !/Routerboard\.com/ {print $1}' | grep -E '^([0-9]{1,3}\.){3}[0-9]{1,3}$')
 # sudo arp-scan --interface=enp36s0 --localnet | awk '/^[0-9]/ && !/Routerboard\.com/ {print $1}' | grep -E '^([0-9]{1,3}\.){3}[0-9]{1,3}$'
 
 # Print the list to verify
