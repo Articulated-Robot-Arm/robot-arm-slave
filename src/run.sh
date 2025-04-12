cac19053a6efb83fe9602c31a9e00a7b673e9861
carwyn987 <carwyncollinsworth@gmail.com>
2025-04-12 14:59:37 -0700

    Add docker execution to playbook

--- src/run.sh
+++ src/run.sh
@@ -5,7 +5,7 @@ docker run --rm -it \
   -e ROS_DOMAIN_ID=42 \
   --net=host \
   --entrypoint /bin/bash \
-  -v /home/pi_node1/dev/robot-arm-slave/:/robot-arm-slave \
+  -v /opt/robot-arm-slave/:/robot-arm-slave \
   --device /dev/gpiomem \
   --device /dev/mem \
   --privileged \
