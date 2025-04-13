e130691c536f1c16462e6e6d09a610682f895acf
carwyn987 <carwyncollinsworth@gmail.com>
2025-04-13 14:53:47 -0700

    Remove docker upload

--- src/build.sh
+++ src/build.sh
@@ -4,4 +4,4 @@ fi
 
 sudo docker build -t carwyn987/robot-arm-slave:$VERSION .
 # Should automate (jenkins?) to ensure unique version numbers
-sudo docker push carwyn987/robot-arm-slave:$VERSION
+#sudo docker push carwyn987/robot-arm-slave:$VERSION
