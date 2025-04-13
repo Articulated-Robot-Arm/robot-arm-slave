6cd3f87c0b741124729b1e8f7745feaf25e5c600
carwyn987 <carwyncollinsworth@gmail.com>
2025-04-13 14:31:59 -0700

    Update to ensure slave builds

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,12 @@
 source /opt/ros/humble/setup.bash
 cd /opt/ros2_ws/
-source install/setup.bash
-ros2 run py_pubsub talker
+colcon build --packages-select servo
+pip install RPi.GPIO
+
+env -i /bin/bash  -c '
+  export HOME=$PWD;
+  export RCUTILS_LOGGING_DIRECTORY=$PWD/log/;
+  source /opt/ros/humble/setup.bash;
+  source install/setup.bash;
+  ros2 run servo servo;
+'
--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-docker run --rm -d \
+sudo docker run -d \
   -e ROS_LOCALHOST_ONLY=0 \
   -e ROS_DOMAIN_ID=42 \
   --net=host \
-  --entrypoint /bin/bash \
   -v /opt/robot-arm-slave/:/robot-arm-slave \
   --device /dev/gpiomem \
   --device /dev/mem \
   --privileged \
   --volume /sys/class/gpio:/sys/class/gpio \
-  carwyn987/robot-arm-slave:arm64v8-1.0.0
+  carwyn987/robot-arm-slave:1.0.0
