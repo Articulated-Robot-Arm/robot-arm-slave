3b5bcb1f239077b5bcbd986acb1b053252a5a2e2
carwyn987 <carwyncollinsworth@gmail.com>
2025-04-27 16:30:58 -0700

    Add event detector back in for testing

--- src/ros2_ws/src/servo/servo/Encoder.py
+++ src/ros2_ws/src/servo/servo/Encoder.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import threading
+import signal
 import time
+import sys
 import os
 
 class Encoder:
@@ -22,15 +24,55 @@ class Encoder:
         self.prev_state_a = GPIO.input(pin_a)
         self.prev_state_b = GPIO.input(pin_b)
 
-        self.lock = threading.Lock()
+        GPIO.add_event_detect(pin_a, GPIO.FALLING, callback=self.encoder_callback_a, bouncetime=100)
+        GPIO.add_event_detect(pin_b, GPIO.FALLING, callback=self.encoder_callback_b, bouncetime=100)
 
+        
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.pause()
+    
+    def signal_handler(sig, frame):
+        GPIO.cleanup()
+        sys.exit()
+
+        # self.lock = threading.Lock()
         # Start polling thread
-        logger.info("Starting polling thread")
-        self._running = True
-        self.poll_thread = threading.Thread(target=self._poll_encoder)
-        self.poll_thread.daemon = True
-        self.poll_thread.start()
-        logger.info("Polling thread started")
+        # logger.info("Starting polling thread")
+        # self._running = True
+        # self.poll_thread = threading.Thread(target=self._poll_encoder)
+        # self.poll_thread.daemon = True
+        # self.poll_thread.start()
+        # logger.info("Polling thread started")
+
+    def encoder_callback_a(self, channel):
+        self.encoder_callback(channel, a=True) 
+
+    def encoder_callback_b(self, channel):
+        self.encoder_callback(channel, a=False) 
+    
+    def encoder_callback(self, channel, a):
+        if a == True:
+            state_a = GPIO.LOW if self.prev_state_a == GPIO.HIGH else GPIO.HIGH
+            state_b = self.prev_state_b
+        else:
+            state_a = self.prev_state_a
+            state_b = GPIO.LOW if self.prev_state_b == GPIO.HIGH else GPIO.HIGH
+
+        with self.lock:
+            if self.prev_state_a == GPIO.LOW and state_a == GPIO.HIGH:  # Rising edge
+                if state_b == GPIO.LOW:
+                    self.encoder_ticks += 1
+                else:
+                    self.encoder_ticks -= 1
+            elif self.prev_state_a == GPIO.HIGH and state_a == GPIO.LOW:  # Falling edge
+                if state_b == GPIO.HIGH:
+                    self.encoder_ticks += 1
+                else:
+                    self.encoder_ticks -= 1
+
+            self.prev_state_a = state_a
+            self.prev_state_b = state_b
+
 
     def _poll_encoder(self):
         while self._running:
